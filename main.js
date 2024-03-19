53a9d1478292030a33aef92924b73a91206e4561
umirbyek <umirbyek0731@gmail.com>
2024-03-19 15:18:03 +0800

    dd

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-gsap.from(".ELEMENT", { rotation: 360, x: 100, duration: 2 });
+gsap.from(".ELEMENT", { x: 100, duration: 2 });
 
 // target the element with a class of "blue" - rotate and move FROM 100px to the left, TO 100px to the right over the course of 1 second. 
 
-gsap.from(".IMAGE", { x: -200, duration: 2,rotation:-360});
+gsap.from(".IMAGE", { x: -200, duration: 2});
 gsap.fromTo(".ICON1", {x: -100},{ x: -10, duration: 2});
 gsap.fromTo(".ICON3", {x: 200},{ x: -10, duration: 2});
 gsap.fromTo(".ICON2",{x: 10},{ x: -10, duration: 2,rotation:360});
